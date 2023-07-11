8c06c921bddd40807ba85e17cdd4736e86615fb4
Quiniou <quiniou@macbook-air-de-quiniou.home>
2023-07-11 12:04:50 +0200

    updates to deploy to heroku

--- src/app/services/gestion.service.ts
+++ src/app/services/gestion.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import {Appartement} from "../models/appartement";
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GestionService {
 
-  private apiUrl = '/api/appartements'; // Remplacez par l'URL de votre API
-
+  private apiUrl = `${environment.apiUrl}/appartements`;
   constructor(private http: HttpClient) { }
 
   getAppartements(): Observable<Appartement[]> {
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false, // true pour l'environnement de production
+  apiUrl: 'https://gestion-quin-06f134c36699.herokuapp.com/api' // URL de l'API du back-end
+};
