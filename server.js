56c6988e444315dca507ff744200ceb183c532f9
Quiniou <quiniou@macbook-air-de-quiniou.home>
2023-07-11 12:07:53 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.static('./dist/'));
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/'}
