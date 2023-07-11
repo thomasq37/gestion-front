2395775fdce23de967c4e4d0d7737edc733e91fd
Quiniou <quiniou@macbook-air-de-quiniou.home>
2023-07-11 12:30:51 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-app.use(cors({
-  origin: 'https://gestion-quin-06f134c36699.herokuapp.com/',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
 app.use(express.static('./dist/'));
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/'}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,3 @@
-
-
-
 export const environment = {
   production: false, // true pour l'environnement de production
   apiUrl: 'https://gestion-quin-06f134c36699.herokuapp.com/api' // URL de l'API du back-end
