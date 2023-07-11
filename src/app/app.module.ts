57abe7d2c727a888183aa9441c39c596b7ef358a
Quiniou <quiniou@macbook-air-de-quiniou.home>
2023-07-11 23:03:01 +0200

    Update slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import {AppartementListComponent} from "./components/appartement-list/appartement-list.component";
 import {AppartementItemComponent} from "./components/appartement-item/appartement-item.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     AppartementListComponent,
-    AppartementItemComponent
+    AppartementItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/appartement-item/appartement-item.component.ts
+++ src/app/components/appartement-item/appartement-item.component.ts
@@ -2,15 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Appartement, FrequenceFrais} from "../../models/appartement";
 import {GestionService} from "../../services/gestion.service";
-
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-appartement-item',
   templateUrl: './appartement-item.component.html',
   styleUrls: ['./appartement-item.component.scss']
 })
 export class AppartementItemComponent implements OnInit{
+  images: string[] = []; // Tableau des chemins d'accès aux images
   appartement!: Appartement;
-
+  currentIndex = 0;
   constructor(private gestionService: GestionService, private route: ActivatedRoute) {
   }
 
@@ -20,6 +21,7 @@ export class AppartementItemComponent implements OnInit{
       this.gestionService.getAppartementById(appartementId)
         .subscribe(appartement => {
           this.appartement = appartement;
+          this.images = appartement.images;
         });
       // Ici, vous devez implémenter la logique pour récupérer l'appartement avec l'ID spécifié
       // par exemple, en utilisant un service ou une requête HTTP
