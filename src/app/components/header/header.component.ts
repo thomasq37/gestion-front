e7b90b57f45aaacdba709bf632bb747f15cb2cd2
Quiniou <quiniou@macbook-air-de-quiniou.home>
2023-10-06 19:22:47 +0200

    Added go back

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  constructor(private authService: AuthService) { }
+export class HeaderComponent implements OnInit{
+  constructor(private authService: AuthService, private location: Location) { }
   menuOpen: boolean = false;
+  isDragging = false;
+  @Input() goBackIsHidden: boolean = false
+  onDragStart(event: any): void {
+    this.isDragging = true;
+  }
+
+  onDragEnd(event: any): void {
+    this.isDragging = false;
+  }
+
+  onDragOver(event: any): void {
+    event.preventDefault();
+  }
+
+  ngOnInit(): void {
+    // Lors de l'initialisation, vérifiez la position précédemment enregistrée
+    this.setButtonPositionFromLocalStorage();
+  }
+
+  setButtonPositionFromLocalStorage(): void {
+    const button = document.querySelector('button');
+    const position = localStorage.getItem('buttonPosition');
+
+    if (button && position === 'right') {
+      button.classList.add('switched');
+    } else if (button) {
+      button.classList.remove('switched');
+    }
+  }
+
+  onDrop(event: any): void {
+    event.preventDefault();
+    const button = document.querySelector('button');
+    if (this.isDragging && button) {
+      if (button.classList.contains('switched')) {
+        button.classList.remove('switched');
+        localStorage.setItem('buttonPosition', 'left');
+      } else {
+        button.classList.add('switched');
+        localStorage.setItem('buttonPosition', 'right');
+      }
+    }
+  }
+
+
   isLoggedIn(): boolean {
     return !!localStorage.getItem('auth_token');
   }
@@ -23,4 +68,7 @@ export class HeaderComponent {
     this.menuOpen = !this.menuOpen;
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
