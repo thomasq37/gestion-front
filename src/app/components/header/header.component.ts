64324b484d770b3e329a78c6151566f8c91fd9e1
Thomas Quiniou <thomasquiniou@MacBook-Air-de-Thomas.local>
2024-11-21 17:26:28 +0100

    Fix header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AuthService} from "../../services/auth.service";
-import {NavigationService} from "../../services/navigation.service";
+import { Component, Input, OnInit, HostListener } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { NavigationService } from "../../services/navigation.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
+  menuOpen: boolean = false;
+  isFixed: boolean = false; // Propriété pour le header fixe
+  lastScrollPosition: number = 0; // Garde en mémoire la dernière position de scroll
+
+  @Input() goBackIsHidden: boolean = false;
+
   constructor(
     private authService: AuthService,
-    private navigationService: NavigationService) { }
-  menuOpen: boolean = false;
-  @Input() goBackIsHidden: boolean = false
+    private navigationService: NavigationService
+  ) { }
+
   ngOnInit(): void {
   }
+
   isLoggedIn(): boolean {
     return !!localStorage.getItem('auth_token');
   }
+
   onLogout(): void {
     this.authService.logout();
   }
@@ -25,17 +33,18 @@ export class HeaderComponent implements OnInit{
   onLogin() {
     this.authService.loginRedirection();
   }
+
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
   }
 
   goBack() {
     const currentUrl = window.location.href;
-    const host = window.location.origin; // Récupère l'hôte actuel (http://localhost:4200 ou autre en prod)
+    const host = window.location.origin;
     if (new RegExp(`${host}/appartement/\\d+/\\w+`).test(currentUrl)) {
       const lastSegment = currentUrl.substring(currentUrl.lastIndexOf('/') + 1);
       if (lastSegment === 'description') {
-        this.navigationService.sendNavigationConfirmation(); // ou false en fonction de votre logique
+        this.navigationService.sendNavigationConfirmation();
       } else {
         window.location.href = currentUrl.substring(0, currentUrl.lastIndexOf('/'));
       }
@@ -43,5 +52,13 @@ export class HeaderComponent implements OnInit{
     else if (new RegExp(`${host}/appartement/\\d+`).test(currentUrl)) {
       window.location.href = host;
     }
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    this.isFixed = currentScrollPosition > this.lastScrollPosition;
+    this.lastScrollPosition = currentScrollPosition;
   }
 }
