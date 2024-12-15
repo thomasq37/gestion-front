97f45cef7706a6c0af3e8e698785c62ff3fe85a5
Thomas Quiniou <thomasquiniou@MacBook-Air-de-Thomas.local>
2024-12-15 12:58:13 +0100

    Fix style et repsonsive

--- src/app/components/v2/periode-location/periode-location-table/periode-location-table.component.ts
+++ src/app/components/v2/periode-location/periode-location-table/periode-location-table.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {PeriodeDeLocationDTO} from "../../../../models/v2/entites/PeriodeDeLocation/PeriodeDeLocationDTO.model";
 
 @Component({
@@ -6,10 +6,17 @@ import {PeriodeDeLocationDTO} from "../../../../models/v2/entites/PeriodeDeLocat
   templateUrl: './periode-location-table.component.html',
   styleUrls: ['./periode-location-table.component.scss']
 })
-export class PeriodeLocationTableComponent {
+export class PeriodeLocationTableComponent implements OnInit {
   @Input() periodesDeLocation!: PeriodeDeLocationDTO[];
 
-  modifierPeriodesDeLocation() {
+  ngOnInit(): void {
+    this.periodesDeLocation.sort((a, b) => {
+      const dateA = new Date(a.dateDeDebut).getTime();
+      const dateB = new Date(b.dateDeDebut).getTime();
+      return dateB - dateA;
+    });
+  }
 
+  modifierPeriodesDeLocation() {
   }
 }
